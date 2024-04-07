c120dbdf675d3942822313c7126ffa2d6cabcc71
Chrome-TRex <166300629+Chrome-TRex@users.noreply.github.com>
2024-04-07 14:33:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -118,7 +118,7 @@
         MAX_CLOUDS: 6,
         MAX_OBSTACLE_LENGTH: 3,
         MAX_OBSTACLE_DUPLICATION: 2,
-        MAX_SPEED: 11,
+        MAX_SPEED: 9,
         MIN_JUMP_HEIGHT: 35,
         MOBILE_SPEED_COEFFICIENT: 1.2,
         RESOURCE_TEMPLATE_ID: 'audio-resources',
